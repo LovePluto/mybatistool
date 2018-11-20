e4dfc9d94c012551b78e8db7a7ba88099cfa6b31
wangyuheng <yuheng.wang@downjoy.com>
2018-11-20 14:00:53 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mybatistool
-mybatis 自动成成工具
+## mybatis 自动成成工具
+- 修改 sql.xml 对应的配置文件
--- src/main/java/com/wyh/get/UserMapper.java
+++ src/main/java/com/wyh/get/UserMapper.java
@@ -1,16 +1,11 @@
 package com.wyh.get;
 
-import java.util.List;
-
 public interface UserMapper {
-    long countByExample(UserExample example);
 
-    int deleteByExample(UserExample example);
 
     int insert(User record);
 
     int insertSelective(User record);
 
-    List<User> selectByExample(UserExample example);
 
 }
